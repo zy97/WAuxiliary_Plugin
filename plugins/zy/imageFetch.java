eda1da7eff85e9300fb5dd70fc5bca0425769ae7
bomky <bom_ky@Outlook.com>
2025-08-24 11:48:02 +0800

    update

---
+++ plugins/zy/imageFetch.java
@@ -0,0 +1,49 @@
+import org.json.JSONObject;
+import java.io.File;
+import me.hd.wauxv.plugin.api.callback.PluginCallBack;
+
+boolean onLongClickSendBtn(String text) {
+    String apiUrl = null;
+    String fileName = null;
+    if (text.startsWith("/imageFetch ")) {
+        String str = text.substring(12);
+        apiUrl = "http://10.144.144.100:17619/" + str;
+        fileName = "imageFetch.jpg";
+    } else {
+        return false;
+    }
+
+    final String finalFileName = fileName;
+
+    get(apiUrl, null, new PluginCallBack.HttpCallback() {
+        public void onSuccess(int respCode, String respContent) {
+            sendText(getTargetTalker(), respContent);
+            return;
+            JSONObject json = new JSONObject(respContent);
+            JSONArray images = json.getJSONArray("images");
+            for (int i = 0; i < images.length(); i++) {
+                String url = images.getString(i); // Get each URL from the array
+                download(url, pluginDir + "/" + finalFileName + "_" + i, null, new PluginCallBack.DownloadCallback() {
+                    public void onSuccess(File file) {
+                        sendImage(getTargetTalker(), file.getAbsolutePath(), "wxe3ad19e142df87b3");
+                    }
+
+                    public void onError(Exception e) {
+                        sendText(getTargetTalker(), "下载异常:" + e.getMessage());
+                    }
+                });
+            }
+            JSONArray images = json.getJSONArray("videos");
+            for (int i = 0; i < images.length(); i++) {
+                String url = images.getString(i);
+                sendText(getTargetTalker(), "视频地址:" + url);
+            }
+        }
+
+        public void onError(Exception e) {
+            sendText(getTargetTalker(), "API错误:" + e.getMessage());
+        }
+    });
+
+    return true;
+}
